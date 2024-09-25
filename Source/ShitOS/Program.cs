ae7449a6602cf442f468d6c09fe810d033b83160
WithsCat <da18.06.03@yandex.ru>
2024-09-25 08:35:36 +0300

    Initial template

---
+++ Source/ShitOS/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ShitOS.Application;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<ApplicationRouting>(".app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
